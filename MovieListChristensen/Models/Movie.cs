b6f79639feb776774e9ac9b4c06e201e82e84d24
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-09-02 10:54:08 -0500

    Added Movie class and class to insert data

---
+++ MovieListChristensen/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieListChristensen.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        [Required(ErrorMessage = "Please enter the Name of the Movie")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the year the Movie came out.")]
+        [Range(1889, 2050, ErrorMessage ="Year must be between 1889 and 2024")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Rating")]
+        [Range(1, 5, ErrorMessage = " Ratings are on the 5 star scale")]
+        public int Rating { get; set; }
+
+        public string Slug =>
+            Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
+
+    }
+}

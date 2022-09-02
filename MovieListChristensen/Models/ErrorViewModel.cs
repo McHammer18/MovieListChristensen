9dc01fb9f8e87bd2bb591bbd56a827e01bdcb372
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-09-02 10:20:25 -0500

    Add project files.

---
+++ MovieListChristensen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieListChristensen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
